e4ad5ff5395974c193daffc4445ecaf536bea093
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2025-02-15 13:08:40 +0400

    feat: add test for download button

--- src/components/Flyout/Flyout.tsx
+++ src/components/Flyout/Flyout.tsx
@@ -44,13 +44,20 @@ const Flyout = () => {
   return (
     bookmarkedCards.length > 0 && (
       <div data-testid={'flyout'}>
-        <a ref={linkRef} href={link.href} download={link.download} />
+        <a
+          ref={linkRef}
+          href={link.href}
+          download={link.download}
+          data-testid={'flyout-link'}
+        />
         <div className="flyout">
           <span>{flyoutText}</span>
           <button onClick={() => unselectAll()} data-testid={'flyout-unselect'}>
             Unselect all
           </button>
-          <button onClick={() => download()}>Download</button>
+          <button onClick={() => download()} data-testid={'flyout-download'}>
+            Download
+          </button>
         </div>
       </div>
     )
--- src/tests/Flyout.test.tsx
+++ src/tests/Flyout.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { describe, it, expect } from 'vitest';
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import DetailsPage from '../pages/DetailsPage/DetailsPage';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
@@ -30,73 +30,80 @@ describe('Flyout Component', () => {
   it('Should render flyout component if character was bookmarked', async () => {
     renderWithRouter();
 
-    const bookmarks = await waitFor(
-      () => {
-        return screen.findAllByTestId('bookmark');
-      },
-      {
-        timeout: 50000,
-      }
-    );
-
-    await act(async () => {
-      await userEvent.click(bookmarks[0]);
+    const bookmarks = await waitFor(() => {
+      return screen.findAllByTestId('bookmark');
     });
+
+    await userEvent.click(bookmarks[0]);
 
     const flyout = screen.getByTestId('flyout');
     expect(flyout).toBeInTheDocument();
-    await act(async () => {
-      await userEvent.click(bookmarks[0]);
-    });
+    await userEvent.click(bookmarks[0]);
   });
 
   it('Should remove flyout if nothing was bookmarked', async () => {
     renderWithRouter();
-    const bookmarks = await waitFor(
-      () => {
-        return screen.findAllByTestId('bookmark');
-      },
-      {
-        timeout: 50000,
-      }
-    );
-    await act(async () => {
-      await userEvent.click(bookmarks[0]);
+    const bookmarks = await waitFor(() => {
+      return screen.findAllByTestId('bookmark');
     });
+    await userEvent.click(bookmarks[0]);
 
     const flyout = screen.getByTestId('flyout');
     expect(flyout).toBeInTheDocument();
 
-    await act(async () => {
-      await userEvent.click(bookmarks[0]);
-    });
+    await userEvent.click(bookmarks[0]);
 
     expect(flyout).not.toBeInTheDocument();
   });
 
   it('Should remove flyout if unselect all button was clicked', async () => {
     renderWithRouter();
-    const bookmarks = await waitFor(
-      () => {
-        return screen.findAllByTestId('bookmark');
-      },
-      {
-        timeout: 50000,
-      }
-    );
-    await act(async () => {
-      await userEvent.click(bookmarks[0]);
+    const bookmarks = await waitFor(() => {
+      return screen.findAllByTestId('bookmark');
     });
+
+    await userEvent.click(bookmarks[0]);
 
     const flyout = screen.getByTestId('flyout');
     expect(flyout).toBeInTheDocument();
 
     const unselectButton = screen.getByTestId('flyout-unselect');
+    await userEvent.click(unselectButton);
 
-    await act(async () => {
-      await userEvent.click(unselectButton);
+    expect(flyout).not.toBeInTheDocument();
+  });
+
+  it('Should generate csv file with number of selected items if download button was clicked', async () => {
+    URL.createObjectURL = vi.fn(() => 'https:/pdf.com');
+    const spyClick = vi.fn();
+    HTMLAnchorElement.prototype.click = vi.fn(() => spyClick());
+
+    renderWithRouter();
+    const bookmarks = await waitFor(() => {
+      return screen.findAllByTestId('bookmark');
     });
 
+    await userEvent.click(bookmarks[0]);
+
+    const flyout = screen.getByTestId('flyout');
+    expect(flyout).toBeInTheDocument();
+
+    const downloadButton = screen.getByTestId('flyout-download');
+    const flyoutLink = screen.getByTestId('flyout-link');
+
+    await userEvent.click(downloadButton);
+    expect(flyoutLink).toHaveAttribute('download', '1_character.csv');
+    expect(spyClick).toHaveBeenCalledTimes(1);
+
+    await userEvent.click(bookmarks[1]);
+    await userEvent.click(downloadButton);
+    expect(flyoutLink).toHaveAttribute('download', '2_characters.csv');
+    expect(spyClick).toHaveBeenCalledTimes(2);
+
+    vi.resetAllMocks();
+
+    const unselectButton = screen.getByTestId('flyout-unselect');
+    await userEvent.click(unselectButton);
     expect(flyout).not.toBeInTheDocument();
   });
 });
